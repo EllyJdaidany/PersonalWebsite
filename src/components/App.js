407f2df3befb1dcb4ccb7b95843d66c2803d7197
unknown <ellyjda@gmail.com>
2020-05-17 02:09:27 -0500

    structure set up

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Wow we have to set this shit up
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
